f28632e4f7ea31b64863c17282db68dc567dcaa0
Ly Hương <lyhng.rika@gmail.com>
2025-12-24 14:39:24 +0900

    Display user and copyright in footer

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,3 +1,18 @@
 <template>
-  <UFooter />
+  <UFooter class="border-t border-t-accented">
+    <div class="flex flex-col justify-start items-center">
+      <div>
+        <p v-if="user">
+          Logged in as <span>{{ user.email }}</span>
+        </p>
+      </div>
+      <div class="text-center py-4">
+        &copy; {{ new Date().getFullYear() }} Huyễn Cảnh Lưu Ly. All rights reserved.
+      </div>
+    </div>
+  </UFooter>
 </template>
+
+<script setup lang="ts">
+const user = useSupabaseUser()
+</script>
