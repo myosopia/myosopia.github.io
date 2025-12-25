da6638bad97c1105d84289b849c7d1b82e81b40c
Ly Hương <lyhng.rika@gmail.com>
2025-12-25 14:10:07 +0900

    Update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,8 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // Your custom configs here
   {
+    files: ['**/*.vue'],
     rules: {
       'vue/no-v-html': 'off',
     },
