2b73fa2c4d459439a59c9084797e2f6f0a1d8804
Ly Hương <lyhng.rika@gmail.com>
2025-12-25 10:44:31 +0900

    Update supabase and workflow config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,4 +30,8 @@ export default defineNuxtConfig({
       { code: 'ja', name: '日本語', file: 'ja.json' },
     ],
   },
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+  },
 })
