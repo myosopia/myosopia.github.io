c17310ba88cd14cac11798370b014e810a593dd5
Ly Hương <lyhng.rika@gmail.com>
2025-12-26 12:51:12 +0900

    Add dictionary drawer

--- app/pages/blog/[...slug].vue
+++ app/pages/blog/[...slug].vue
@@ -1,18 +1,43 @@
 <template>
   <UPage>
     <UPageBody>
-      <ContentRenderer
-        v-if="post"
-        :value="post"
-        class="px-4 max-w-3xl mx-auto"
-      />
+      <UContextMenu :items="contextMenuItems">
+        <ContentRenderer
+          v-if="post"
+          :value="post"
+          class="px-4 max-w-3xl mx-auto"
+        />
+        <UDrawer
+          v-model:open="dictionaryOpen"
+          direction="right"
+          class="w-64 sm:w-96"
+        >
+          <template #content>
+            <div class="p-4">
+              <DictionaryLookup />
+            </div>
+          </template>
+        </UDrawer>
+      </UContextMenu>
     </UPageBody>
   </UPage>
 </template>
 
 <script setup lang="ts">
+import type { ContextMenuItem } from '@nuxt/ui'
+
 const route = useRoute()
 const { data: post } = await useAsyncData(route.path, () => {
   return queryCollection('blog').path(route.path).first()
 })
+const dictionaryOpen = ref(false)
+const contextMenuItems = ref<ContextMenuItem[]>([
+  {
+    label: 'Dictionary Lookup',
+    icon: 'i-lucide-book-open',
+    onSelect: () => {
+      dictionaryOpen.value = true
+    },
+  },
+])
 </script>
