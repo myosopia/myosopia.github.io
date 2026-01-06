0ef0145e73ecef7d462dc491f252297497e48e83
Ly Hương <lyhng.rika@gmail.com>
2026-01-06 16:01:37 +0900

    Use prettier for formatting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,49 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@nuxtjs/i18n',
-    '@nuxtjs/supabase',
-  ],
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  compatibilityDate: '2025-07-15',
-  nitro: {
-    prerender: {
-      failOnError: false,
-    },
-  },
-  eslint: {
-    config: {
-      stylistic: true,
-    },
-  },
-  i18n: {
-    defaultLocale: 'zh-CN',
-    locales: [
-      { code: 'en', name: 'English', file: 'en.json' },
-      { code: 'vi', name: 'Tiếng Việt', file: 'vi.json' },
-      { code: 'zh-CN', name: '简体中文', file: 'zh.json' },
-      { code: 'ja', name: '日本語', file: 'ja.json' },
-    ],
-  },
-  icon: {
-    clientBundle: {
-      icons: [
-        'lucide:x',
-        'lucide:check',
-        'lucide:plus',
-        'lucide:minus',
-      ],
-      scan: true,
-    },
-  },
-  supabase: {
-    redirect: false,
-    url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_KEY,
-  },
+	modules: [
+		'@nuxt/content',
+		'@nuxt/eslint',
+		'@nuxt/image',
+		'@nuxt/ui',
+		'@nuxtjs/i18n',
+		'@nuxtjs/supabase',
+	],
+	devtools: { enabled: true },
+	css: ['~/assets/css/main.css'],
+	compatibilityDate: '2025-07-15',
+	nitro: {
+		prerender: {
+			failOnError: false,
+		},
+	},
+	i18n: {
+		defaultLocale: 'zh-CN',
+		locales: [
+			{ code: 'en', name: 'English', file: 'en.json' },
+			{ code: 'vi', name: 'Tiếng Việt', file: 'vi.json' },
+			{ code: 'zh-CN', name: '简体中文', file: 'zh.json' },
+			{ code: 'ja', name: '日本語', file: 'ja.json' },
+		],
+	},
+	icon: {
+		clientBundle: {
+			icons: ['lucide:x', 'lucide:check', 'lucide:plus', 'lucide:minus'],
+			scan: true,
+		},
+	},
+	supabase: {
+		redirect: false,
+		url: process.env.SUPABASE_URL,
+		key: process.env.SUPABASE_KEY,
+	},
 })
