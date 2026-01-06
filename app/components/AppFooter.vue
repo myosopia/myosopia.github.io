f1bc9fdd8384e3ddac7ab9ab0f53fafeed966f31
Ly Hương <lyhng.rika@gmail.com>
2026-01-06 10:02:48 +0900

    Add sign out button

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -2,10 +2,20 @@
   <UFooter>
     <div class="flex flex-col justify-start items-center space-y-4">
       <div>────────୨ৎ────────</div>
-      <div>
-        <p v-if="user">
-          Logged in as <span>{{ user.email }}</span>
-        </p>
+      <div
+        v-if="user"
+        class="flex items-center"
+      >
+        You are logged in
+        <UButton
+          size="sm"
+          variant="ghost"
+          color="neutral"
+          icon="i-lucide-log-out"
+          @click=" () => {
+            supabase.auth.signOut()
+          }"
+        />
       </div>
       <div class="text-center">
         &copy; {{ new Date().getFullYear() }} Huyễn Cảnh Lưu Ly. All rights reserved.
@@ -16,4 +26,5 @@
 
 <script setup lang="ts">
 const user = useSupabaseUser()
+const supabase = useSupabaseClient()
 </script>
