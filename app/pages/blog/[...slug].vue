8ac59c57041308b58e027f5a3090df7aacdd14b4
Ly Hương <lyhng.rika@gmail.com>
2026-07-08 16:00:10 +0900

    Add toc to blog

--- app/pages/blog/[...slug].vue
+++ app/pages/blog/[...slug].vue
@@ -1,37 +1,43 @@
 <template>
 	<UPage>
-		<UPageBody>
-			<UContextMenu :items="contextMenuItems" :disabled="!post?.dictionary">
-				<ContentRenderer
-					v-if="post"
-					:value="post"
-					class="px-4 max-w-3xl mx-auto"
-				/>
-				<UDrawer
-					v-model:open="dictionaryDrawerRightOpen"
-					direction="right"
-					class="w-96"
-				>
-					<template #content>
-						<div class="p-4 grow">
-							<DictionaryLookup v-model="dictionaryQuery" />
-						</div>
-					</template>
-				</UDrawer>
-				<UDrawer
-					v-model:open="dictionaryDrawerBottomOpen"
-					direction="bottom"
-					class="h-1/2 min-h-120"
-				>
-					<template #content>
-						<div class="p-4 grow">
-							<DictionaryLookup v-model="dictionaryQuery" />
-						</div>
-					</template>
-				</UDrawer>
-			</UContextMenu>
-			<ScrollToTop />
+		<UPageBody class="relative">
+			<UContainer class="relative">
+				<UContextMenu :items="contextMenuItems" :disabled="!post?.dictionary">
+					<ContentRenderer v-if="post" :value="post" />
+					<UDrawer
+						v-model:open="dictionaryDrawerRightOpen"
+						direction="right"
+						class="w-96"
+					>
+						<template #content>
+							<div class="p-4 grow">
+								<DictionaryLookup v-model="dictionaryQuery" />
+							</div>
+						</template>
+					</UDrawer>
+					<UDrawer
+						v-model:open="dictionaryDrawerBottomOpen"
+						direction="bottom"
+						class="h-1/2 min-h-120"
+					>
+						<template #content>
+							<div class="p-4 grow">
+								<DictionaryLookup v-model="dictionaryQuery" />
+							</div>
+						</template>
+					</UDrawer>
+				</UContextMenu>
+				<div v-if="toc" class="absolute h-full right-4 top-1">
+					<TocModal :toc="toc" />
+				</div>
+			</UContainer>
 		</UPageBody>
+		<ScrollToTop />
+		<template #right>
+			<UPageAside v-if="toc" class="relative">
+				<ComarkToc :toc="toc" />
+			</UPageAside>
+		</template>
 	</UPage>
 </template>
 
@@ -44,6 +50,7 @@ const route = useRoute()
 const { data: post } = await useAsyncData(route.path, () => {
 	return queryCollection('blog').path(route.path).first()
 })
+const toc = computed(() => post.value?.body?.toc)
 const dictionaryDrawerRightOpen = ref(false)
 const dictionaryDrawerBottomOpen = ref(false)
 const contextMenuItems = ref<ContextMenuItem[]>([
