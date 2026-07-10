288708a5f9a979cb2d4fc9e1b954c63dfdd22cea
Ly Hương <lyhng.rika@gmail.com>
2026-07-10 13:59:31 +0900

    Add ProseH2 component

---
+++ app/components/prose/ProseH2.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import H2 from '@nuxt/ui/components/prose/H2.vue'
+</script>
+<template>
+	<H2
+		:ui="{
+			leading: 'top-1/2 -translate-y-1/2',
+		}"
+		class="prose-h2 flex flex-col gap-4 ps-4 py-4"
+		><slot
+	/></H2>
+</template>
+<style lang="css" scoped>
+@reference '~/assets/css/main.css';
+.prose-h2 {
+	&::before {
+		content: '⊹';
+		@apply absolute inline-block text-primary text-base font-bold lg:text-lg top-0 left-0 leading-none;
+	}
+	&:hover::before {
+		@apply animate-ping;
+	}
+}
+</style>
